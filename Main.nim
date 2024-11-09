d0d80e89d2a90528f136f2998f4f5ac6af57bf60
multiplex55 <multiplex55@yahoo.com>
2024-11-08 22:09:38 -0500

    init

---
+++ Main.nim
@@ -0,0 +1,31 @@
+import Day1Mod
+import Day2Mod
+import Day3Mod
+import Day3Mod_Part2
+import Day4Mod
+import Day4Mod_Part2
+
+import strutils, strformat, streams, os, times, tables, sequtils, algorithm
+
+when isMainModule:
+    var startTimeCPU = cpuTime()
+    var startTimeDT = now()
+
+    # echo "Day1Part1 " & $Day1Mod.TrebuchetPart1(os.getCurrentDir() & "\\inputFiles\\day1.txt")
+    # echo "Day1Part2 " & $Day1Mod.TrebuchetPart2(os.getCurrentDir() & "\\inputFiles\\day1.txt")
+    # echo "Day2Part1 " & $Day2Mod.CubeConundrumPart1(os.getCurrentDir() & "\\inputFiles\\day2.txt")
+    # echo "Day2Part2 " & $Day2Mod.CubeConundrumPart2(os.getCurrentDir() & "\\inputFiles\\day2.txt")
+    # echo "Day3Part1_Small " & $Day3Mod.GearRatio(os.getCurrentDir() & "\\inputFiles\\day3_small.txt")
+    #echo "Day3Part1 " & $Day3Mod.GearRatioPart1(os.getCurrentDir() & "\\inputFiles\\day3.txt")
+    #echo "Day3Part2 " & $Day3Mod_Part2.GearRatioPart2(os.getCurrentDir() & "\\inputFiles\\day3_small.txt")
+    #echo "Day4 " & $Day4Mod.Scratchcards(os.getCurrentDir() & "\\inputFiles\\day4_small.txt")
+    #echo "Day4 " & $Day4Mod.Scratchcards(os.getCurrentDir() & "\\inputFiles\\day4.txt")
+    echo "Day4Part2Small " & $Day4Mod_Part2.Scratchcards(os.getCurrentDir() & "\\inputFiles\\day4_small.txt")
+    #echo "Day4Part2Large " & $Day4Mod_Part2.Scratchcards(os.getCurrentDir() & "\\inputFiles\\day4.txt")
+
+    var endTimeCPU = cpuTime()
+    var endTimeDT = now()
+
+    echo endTimeCPU - startTimeCPU
+    echo endTimeDT - startTimeDT
+
