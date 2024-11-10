ccf49022901e6a5df9a2b26974f2933a332c1fa5
multiplex55 <multiplex55@yahoo.com>
2024-11-10 12:33:51 -0500

    day5

--- Main.nim
+++ Main.nim
@@ -4,6 +4,8 @@ import Day3Mod
 import Day3Mod_Part2
 import Day4Mod
 import Day4Mod_Part2
+import Day5Mod
+import Day5Mod_Part2
 
 import strutils, strformat, streams, os, times, tables, sequtils, algorithm
 
@@ -20,8 +22,13 @@ when isMainModule:
     #echo "Day3Part2 " & $Day3Mod_Part2.GearRatioPart2(os.getCurrentDir() & "\\inputFiles\\day3_small.txt")
     #echo "Day4 " & $Day4Mod.Scratchcards(os.getCurrentDir() & "\\inputFiles\\day4_small.txt")
     #echo "Day4 " & $Day4Mod.Scratchcards(os.getCurrentDir() & "\\inputFiles\\day4.txt")
-    echo "Day4Part2Small " & $Day4Mod_Part2.Scratchcards(os.getCurrentDir() & "\\inputFiles\\day4_small.txt")
+    #echo "Day4Part2Small " & $Day4Mod_Part2.Scratchcards(os.getCurrentDir() & "\\inputFiles\\day4_small.txt")
     #echo "Day4Part2Large " & $Day4Mod_Part2.Scratchcards(os.getCurrentDir() & "\\inputFiles\\day4.txt")
+
+
+    #echo "Day5Part1_Small " & $Day5Mod.IfYouGiveASeedAFertilizer(os.getCurrentDir() & "\\inputFiles\\day5_small.txt")
+    #echo "Day5Part1 " & $Day5Mod.IfYouGiveASeedAFertilizer(os.getCurrentDir() & "\\inputFiles\\day5.txt")
+    echo "Day5Part2 " & $Day5Mod_Part2.IfYouGiveASeedAFertilizer(os.getCurrentDir() & "\\inputFiles\\day5.txt")
 
     var endTimeCPU = cpuTime()
     var endTimeDT = now()
